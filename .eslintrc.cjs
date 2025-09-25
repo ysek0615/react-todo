10f779c7e618422d05c093fdd0ea979db05f9f94
ysek0615 <y.sekcal@gmail.com>
2025-09-25 21:00:54 +0900

    add：ESLintとPrettierの追加

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: ['react', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+  },
+};
